a047f29b497a98fedd8940a88b4feecf893b95c2
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-10-04 15:52:45 +0200

    Uncomment kubernetes resources for container logs

--- main.tf
+++ main.tf
@@ -440,35 +440,35 @@ resource "kubernetes_cluster_role_binding" "dashboardviewonly" {
 # Container logs for Azure
 #
 
-resource "kubernetes_cluster_role" "containerlogs" {
-  metadata {
-    name = "containerhealth-log-reader"
-  }
+# resource "kubernetes_cluster_role" "containerlogs" {
+#   metadata {
+#     name = "containerhealth-log-reader"
+#   }
 
-  rule {
-    api_groups = [""]
-    resources  = ["pods/log"]
-    verbs      = ["get"]
-  }
-}
+#   rule {
+#     api_groups = [""]
+#     resources  = ["pods/log"]
+#     verbs      = ["get"]
+#   }
+# }
 
-resource "kubernetes_cluster_role_binding" "containerlogs" {
-  metadata {
-    name = "containerhealth-read-logs-global"
-  }
+# resource "kubernetes_cluster_role_binding" "containerlogs" {
+#   metadata {
+#     name = "containerhealth-read-logs-global"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.containerlogs.metadata.0.name
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.containerlogs.metadata.0.name
+#   }
 
-  subject {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "User"
-    name      = "clusterUser"
-  }
-}
+#   subject {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "User"
+#     name      = "clusterUser"
+#   }
+# }
 
 #
 # Tiller service account
