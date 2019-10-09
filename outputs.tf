9bad9e12491bf98e60f277eb53ca153ad507537e
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-10-09 12:40:28 +0200

    Remove service accounts from output completely

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "host" {
   description = "The Kubernetes cluster server host."
   value       = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
 }
-
-output "service_account_keys" {
-  description = "Map of all service accounts created and their keys."
-  value       = zipmap(var.service_accounts.*.name, data.kubernetes_secret.sa.*.data)
-  sensitive   = true
-}
