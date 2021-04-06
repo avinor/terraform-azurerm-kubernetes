fb0c1f08e533d33aa06f293778a3242c77376c5d
Yngve Hansen <yngve.hansen2@avinor.no>
2021-04-06 06:51:16 +0200

    Add orchestration_version for node pools

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,28 +1,29 @@
 module "simple" {
-    source = "../../"
+  source = "../../"
 
-    name = "simple"
-    resource_group_name = "simple-aks-rg"
-    location = "westeurope"
-    service_cidr = "10.241.0.0/24"
-    kubernetes_version = "1.13.5"
-    
-    service_principal = {
-        object_id = "00000000-0000-0000-0000-000000000000"
-        client_id = "00000000-0000-0000-0000-000000000000"
-        client_secret = "00000000-0000-0000-0000-000000000000"
-    }
+  name                     = "simple"
+  resource_group_name      = "simple-aks-rg"
+  location                 = "westeurope"
+  service_cidr             = "10.241.0.0/24"
+  kubernetes_version       = "1.18.14"
 
-    azure_active_directory = {
-        client_app_id = "00000000-0000-0000-0000-000000000000"
-        server_app_id = "00000000-0000-0000-0000-000000000000"
-        server_app_secret = "00000000-0000-0000-0000-000000000000"
-    }
+  service_principal = {
+    object_id     = "00000000-0000-0000-0000-000000000000"
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000-0000-0000-0000-000000000000"
+  }
 
-    agent_pools = [
-        {
-            name = "linux"
-            vnet_subnet_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.Network/virtualNetworks/myvnet1"
-        },
-    ]
+  azure_active_directory = {
+    client_app_id     = "00000000-0000-0000-0000-000000000000"
+    server_app_id     = "00000000-0000-0000-0000-000000000000"
+    server_app_secret = "00000000-0000-0000-0000-000000000000"
+  }
+
+  agent_pools = [
+    {
+      name           = "linux"
+      orchestrator_version = "1.18.14"
+      vnet_subnet_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.Network/virtualNetworks/myvnet1"
+    },
+  ]
 }
