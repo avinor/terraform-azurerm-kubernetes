d76767db00981493c4fbd698b38f3785a8f0d3a7
Yngve Hansen <yngve.hansen2@avinor.no>
2021-04-07 08:16:28 +0200

    Fix diagnostic log using metric variable

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ locals {
     storage_account_id = contains(local.diag_resource_list, "Microsoft.Storage") ? var.diagnostics.destination : null
     event_hub_auth_id  = contains(local.diag_resource_list, "Microsoft.EventHub") ? var.diagnostics.destination : null
     metric             = var.diagnostics.metrics
-    log                = var.diagnostics.metrics
+    log                = var.diagnostics.logs
     } : {
     log_analytics_id   = null
     storage_account_id = null
