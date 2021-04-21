7a4339b9fa48255999334b5b9ef642b9ed945f4c
Yngve Hansen <6208039+yngveh@users.noreply.github.com>
2021-04-21 07:02:04 +0200

    Ignore changes role assignmenet scope (#10)
    * Ignore changes for role assignment scope attribute
    
    * Add comment for ignore changes for role assignment scope attribute

--- main.tf
+++ main.tf
@@ -262,6 +262,11 @@ resource "azurerm_role_assignment" "msi" {
   scope                = var.managed_identities[count.index]
   role_definition_name = "Managed Identity Operator"
   principal_id         = var.service_principal.object_id
+
+  // For now ignore changes for scope because of lower/upper case issue with resourceId forces a recreate of this resource.
+  lifecycle {
+    ignore_changes = [scope]
+  }
 }
 
 # Configure cluster
